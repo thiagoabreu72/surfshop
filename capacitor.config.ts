5ffaf166a59ecdea0960e9844f03121902448c4e
Thiago Abreu <thiagoabreu72@gmail.com>
2022-02-19 12:21:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'surfshop',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
